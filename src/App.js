30f24348fc01e943ff468b9ec2f9d10869f35a9c
terryzzzzz <ter2y@Ter2ys-MBP.modem>
2020-11-12 17:24:28 +1100

    add test line for deploy testing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <p className="text-bold text-red-600">Hello world</p>
+      <p className="text-bold text-red-400">Another line has been added</p>
     </div>
   );
 }
